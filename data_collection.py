8ecb5b23833e5e82ddfad6dbd99143ceee3469f2
Rayn Carrillo <113151668+Raynsad@users.noreply.github.com>
2022-10-04 15:57:42 -0400

    Update data_collection.py
    Updated data collection script using the python datetime module and made sure negative values don't occur when finding the total time spent in a system.

--- data_collection.py
+++ data_collection.py
@@ -1,13 +1,15 @@
 # This script will be run from the host VM after all files have been transferred to the host.
 
 import re
+import datetime
 import time
+from datetime import date, timedelta, datetime
 
 ip_addresses = []
 ip_address_pattern = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
 
 # This file name will need to be changed once we determine what we will be naming the auth.log file we transfer, as well as how often we will be running this script.
-with open('../logs/auth.log', 'r') as file:
+with open('auth.log', 'r') as file:
     for line in file:
         if line.find("Accepted password") != -1:
             ip_addresses.append(ip_address_pattern.search(line)[0])
@@ -18,7 +20,7 @@ print(ip_addresses)
 times = {}
 time_pattern = re.compile(r'\b([01]?[0-9]|2[0-3]):([0-5][0-9])(?::([0-9][0-9]))?\b')
 
-with open('../logs/auth.log', 'r') as file:
+with open('auth.log', 'r') as file:
     for line in file:
         if line.find("Accepted password") != -1:
             start_time = time_pattern.search(line)[0]
@@ -27,22 +29,26 @@ with open('../logs/auth.log', 'r') as file:
             start_time = times.get(ip_address_pattern.search(line)[0])
             if start_time != None:
                 end_time = time_pattern.search(line)[0]
-                end_values = end_time.split(':')
-                start_values = start_time.split(':')
-                total_time = ''
-                # Need to figure out how to prevent negative values
-                for value in range(0, 3):
-                    total_time += str(int(end_values[value]) - int(start_values[value]))
-                    total_time += ":"
-                total_time = total_time.rstrip(total_time[-1])
-                times[ip_address_pattern.search(line)[0]] = total_time
+                # Converted our start and end time values to datetime objects
+                end_values = datetime.strptime(end_time, "%H:%M:%S")
+                start_values = datetime.strptime(start_time, "%H:%M:%S")
+                total_time = end_values - start_values
+                # Makes sure there isn't a negative value when subtracting the timestamps
+                if total_time.days < 0:
+                    total_time = timedelta(
+                        days = 0,
+                        seconds = total_time.seconds,
+                        microseconds = total_time.microseconds
+                    )
+                # Puts the total time into "Hour:Minute:Second" format by using the str method
+                times[ip_address_pattern.search(line)[0]] = str(total_time)
 
 # We will need to figure out how we want to store this long term
 print(times)
 
 commands = []
 
-with open('../logs/snoopy.log', 'r') as file:
+with open('snoopy.log', 'r') as file:
     for line in file:
         line = line.rsplit(']: ', 1)[1]
         if line.find("Server listening") == -1:
