a8b848edcdf6acbec87445ef881fe34ec78f4a02
ameliatalbot <ameliatalbot4@gmail.com>
2022-10-11 15:09:32 -0400

    adding container name info to script

--- data_collection.py
+++ data_collection.py
@@ -4,25 +4,28 @@ import re
 import datetime
 import time
 from datetime import date, timedelta, datetime
+import sys
+
+container_name = sys.argv[1]
 
 ip_addresses = []
 ip_address_pattern = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
 
 # This file name will need to be changed once we determine what we will be naming the auth.log file we transfer, as well as how often we will be running this script.
-with open('logs/auth.log', 'r') as file:
+with open(f'logs/{container_name}/auth.log', 'r') as file:
     for line in file:
         if line.find("Accepted password") != -1:
             ip_addresses.append(ip_address_pattern.search(line)[0])
 
 curr_ip_address = ip_addresses.pop()
 
-with open('data/last_ip_address.txt', 'w') as file:
+with open(f'data/{container_name}/last_ip_address.txt', 'w') as file:
     file.write(curr_ip_address)
 
 times = {}
 time_pattern = re.compile(r'\b([01]?[0-9]|2[0-3]):([0-5][0-9])(?::([0-9][0-9]))?\b')
 
-with open('logs/auth.log', 'r') as file:
+with open(f'logs/{container_name}/auth.log', 'r') as file:
     for line in file:
         if line.find("Accepted password") != -1:
             start_time = time_pattern.search(line)[0]
@@ -45,7 +48,7 @@ with open('logs/auth.log', 'r') as file:
                 # Puts the total time into "Hour:Minute:Second" format by using the str method
                 times[ip_address_pattern.search(line)[0]] = str(total_time)
 
-with open('data/times/total_time.txt', 'w') as file:
+with open(f'data/{container_name}/times/total_time.txt', 'w') as file:
     file.write('[\n')
     for line in times:
         file.write(line + ": " + times[line] + ",\n")
@@ -53,13 +56,13 @@ with open('data/times/total_time.txt', 'w') as file:
 
 commands = []
 
-with open('logs/snoopy.log', 'r') as file:
+with open(f'logs/{container_name}/snoopy.log', 'r') as file:
     for line in file:
         line = line.rsplit(']: ', 1)[1]
         if line.find("Server listening") == -1:
             commands.append(line)
 
-with open(f'data/commands/{curr_ip_address}.txt', 'w') as file:
+with open(f'data/{container_name}/commands/{curr_ip_address}.txt', 'w') as file:
     for line in commands:
         file.write(line + "\n")
 
