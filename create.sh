1a8e5a52a22fcf3b2156e58c9431d3f29c5dad02
honey <honey@umd.edu>
2022-10-20 00:03:57 +0000

    think it works?

--- create.sh
+++ create.sh
@@ -1,36 +1,20 @@
 #!/bin/bash
 
-# create 4 honeypots for different warnings banners
-
-# $1 = container-name
-# $2 = external ip address
-# $3 = external network netmask prefix
-# $4 = banner message file
-
-#if [ $# -ne 4 ]
-#then
-#	echo "Usage: ./create.sh [container name] [external ip address] [external network netmask prefix] [banner message]"
-#	exit 1
-#fi
-
 sudo iptables-restore ./iptables.txt
+sudo ./delete.sh
 
-names=( "no_banner" "low_banner" "med_banner" "high_banner" )
-names=( $(shuf -e "${names[@]}")) 
-#arr=( "128.8.238.19" "128.8.238.36" "128.8.238.55" "128.8.238.185" "128.8.238.112" )
+names=( "template_no_banner" "template_low_banner" "template_med_banner" "template_high_banner" )
 arr=( "128.8.238.19" "128.8.238.36" "128.8.238.55" "128.8.238.185")
+arr=( $(shuf -e "${arr[@]}")) 
+pots=( "no_banner" "low_banner" "med_banner" "high_banner" ) 
 
-#length=${#names[@]}
-length=4
+length=1
 for ((j = 0 ; j < $length; j++));
 do
 	
 	n=${names[$j]}
 	ip=${arr[$j]}
-	mask=24
-	echo "$n"
-	echo "$ip"
-
+	mask=32
 	exists=$(sudo lxc-ls -1 | grep -w $n | wc -l)
 	if [ $exists -ne 0 ]
 	then
@@ -43,16 +27,15 @@ do
 	sudo lxc-start -n $n
 
 	# CREATE FAKE ADMIN USER
-	sudo lxc-attach -n $n -- bash -c "sudo useradd -m test -p password"
-	# sudo lxc-attach -n $n -- bash -c "sudo passwd admin"
-	# prompt will appear, input desired password
+	sudo lxc-attach -n $n -- bash -c "sudo useradd -m user"
+	sudo lxc-attach -n $n -- bash -c "echo user:password | sudo chpasswd"
 	
 	# INSTALL SSH
 	sudo lxc-attach -n $n -- bash -c "sudo apt-get install openssh-server"
 	sudo lxc-attach -n $n -- bash -c "sudo systemctl enable ssh --now"
 
 	# ADD HONEY TO CONTAINER
-	sudo cp -r ./fall2021 ./spring2022 /var/lib/lxc/$n/rootfs/home/admin
+	sudo cp -r ./fall2021 ./spring2022 /var/lib/lxc/$n/rootfs/home/user
 
 	# INSTALL SNOOPY KEYLOGGER
 	# logs to /var/log/snoopy.log within the container
@@ -64,21 +47,25 @@ do
 	sudo lxc-attach -n $n -- bash -c "rm -rf ./install-snoopy.* snoopy-*" 
 	sudo lxc-attach -n $n -- bash -c "echo output = file:/var/log/snoopy.log >> /etc/snoopy.ini" 
 
-	# SET UP EXTERNAL IP
-	container_ip=$(sudo lxc-info -n $n -iH)
+	# CREATE WARNING BANNER
+	cat "warnings/$n.txt" | sudo tee -a /var/lib/lxc/$n/rootfs/etc/motd > /dev/null
+	sudo lxc-stop -n $n
+		
+	# CREATE INITIAL COPY OF THE TEMPLATE
+	pot=${pots[$j]}
+	sudo lxc-copy -n $n -N $pot
+	sudo lxc-start -n $pot
+	sudo sleep 30
+	
+	# SET UP COPY's FIREWALL RULES
+	container_ip=$(sudo lxc-info -n $pot -iH)
+	echo "$pot: $container_ip, external: $ip"
 	sudo ip link set enp4s2 up  
 	sudo ip addr add $ip/$mask brd + dev enp4s2
 	sudo iptables --table nat --insert PREROUTING --source 0.0.0.0/0 --destination $ip --jump DNAT --to-destination $container_ip
 	sudo iptables --table nat --insert POSTROUTING --source $container_ip --destination 0.0.0.0/0 --jump SNAT --to-source $ip 
-	sudo ./firewall.sh
-	#sudo ./dit_firewall_rules.sh
-	#sudo ./dit_firewall_rules2.sh
-	# SET UP PORT DROPPING RULES 
-	# sudo iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-	# sudo iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
-
-	# CREATE WARNING BANNER
-	cat "warnings/$n.txt" | sudo tee -a /var/lib/lxc/$n/rootfs/etc/motd > /dev/null
+	sudo ./tailing.sh $pot
 done
+#sudo ./firewall.sh
 
 exit 0
