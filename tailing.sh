de36b342813412a1f0baea2330c05ee333cdd1bc
jana liu <87097023+jalliu@users.noreply.github.com>
2022-10-21 03:39:50 -0400

    removed unecessary code, added comments

--- tailing.sh
+++ tailing.sh
@@ -2,28 +2,21 @@
 
 if [ $# -ne 2 ]
 then
-	echo "Usage ./tailing.sh [container name] [datetime]"
+	echo "Usage: ./tailing.sh [container name] [datetime]"
 	exit 1
 fi
-echo $1
-echo $2
 
 container=$1
 datetime=$2
 scenario=$(echo $1 | cut -d '_' -f1,2)
 ip=$(echo $1 | cut -d '_' -f3)
-# log=$(logs/${scenario}/${datetime}_${container}.log)
 
+# copy contents of container's auth.log to host
 sudo tail -f /var/lib/lxc/${container}/rootfs/var/log/auth.log >> logs/${scenario}/${datetime}_${container}.log &
-
 tailpid=$!
 
-echo "ip:$ip"
-echo "tail:$tailpid"
+echo "ip: $ip, tail: $tailpid"
 echo $tailpid > logs/${scenario}/${ip}_tail.txt
 
+# call script that triggers ./recycle.sh 
 sudo ./inot.sh logs/${scenario}/${datetime}_${container}.log $container &
-
-#sudo tail -f logs/${scenario}/${datetime}_${container}.log | grep "Disconnected from user" -m 1
-
-#sudo ./recycle.sh $container
