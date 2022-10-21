b48726e1934ddeece05c4e173a05483aedbd8b13
honey <honey@umd.edu>
2022-10-21 07:24:58 +0000

    updated to kill all tail, inotifywait, etc. processes

--- delete.sh
+++ delete.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-for i in $(sudo lxc-ls); do lxc-stop $i; done
-for i in $(sudo lxc-ls); do lxc-destroy $i; done
+for i in $(sudo ps | grep inot.sh | cut -d ' ' -f3); do sudo kill $i; done
+for i in $(sudo ps | grep inotifywait | cut -d ' ' -f3); do sudo kill $i; done
+for i in $(sudo ps | grep "tail" | cut -d ' ' -f3); do sudo kill $i; done
+for i in $(sudo lxc-ls); do sudo lxc-stop $i; done
+for i in $(sudo lxc-ls); do sudo lxc-destroy $i -f; done
