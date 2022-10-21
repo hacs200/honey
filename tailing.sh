a4c94e5d5126ce4efb83c9201133d32583dc3e2e
Mia Hsu <55610339+ameliahsu@users.noreply.github.com>
2022-10-21 12:31:03 -0400

    added full paths to fix reboot bug

--- tailing.sh
+++ tailing.sh
@@ -12,11 +12,11 @@ scenario=$(echo $1 | cut -d '_' -f1,2)
 ip=$(echo $1 | cut -d '_' -f3)
 
 # copy contents of container's auth.log to host
-sudo tail -f /var/lib/lxc/${container}/rootfs/var/log/auth.log >> logs/${scenario}/${datetime}_${container}.log &
+sudo tail -f /var/lib/lxc/${container}/rootfs/var/log/auth.log >> /home/honey/logs/${scenario}/${datetime}_${container}.log &
 tailpid=$!
 
 echo "ip: $ip, tail: $tailpid"
-echo $tailpid > logs/${scenario}/${ip}_tail.txt
+echo $tailpid > /home/honey/logs/${scenario}/${ip}_tail.txt
 
 # call script that triggers ./recycle.sh 
-sudo ./inot.sh logs/${scenario}/${datetime}_${container}.log $container &
+sudo /home/honey/inot.sh /home/honey/logs/${scenario}/${datetime}_${container}.log $container &
