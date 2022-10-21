7674a150d509ff59f6071e859962d6c1c87880cb
jana liu <87097023+jalliu@users.noreply.github.com>
2022-10-21 03:36:04 -0400

    added usage message, renamed vars

--- inot.sh
+++ inot.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-while inotifywait -e modify $1; do
+
+if [ $# -ne 2 ]
+then
+	echo "Usage: ./inot.sh [container log file] [container name]"
+fi 
+
+file=$1
+container=$2
+
+while inotifywait -e modify $file; do
 	if cat $1 | grep "Disconnected from user" -q; then
 		echo "*******************************************************************"
-		echo "TRIGGERING RECYCLE SCRIPT"
+		echo "			TRIGGERING RECYCLE SCRIPT"
 		echo "*******************************************************************"
-		sudo ./recycle.sh $2
+		sudo ./recycle.sh $container
 		exit 0
 	fi
 done
