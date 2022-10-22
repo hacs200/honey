ea92dbc0c198876dccd2e5a002b6dad363ccdf31
jana liu <87097023+jalliu@users.noreply.github.com>
2022-10-22 10:10:22 -0400

    refactor code

--- create.sh
+++ create.sh
@@ -7,7 +7,7 @@ sudo iptables-restore /home/honey/iptables.txt
 sudo /home/honey/delete.sh
 sudo /home/honey/firewall.sh
 
-templates=( "template_no_banner" "template_low_banner" "template_med_banner" "template_high_banner" )
+#templates=( "template_no_banner" "template_low_banner" "template_med_banner" "template_high_banner" )
 ips=( "128.8.238.19" "128.8.238.36" "128.8.238.55" "128.8.238.185")
 ips=( $(shuf -e "${ips[@]}")) 
 scenarios=( "no_banner" "low_banner" "med_banner" "high_banner" )
@@ -15,58 +15,67 @@ scenarios=( "no_banner" "low_banner" "med_banner" "high_banner" )
 sudo sysctl -w net.ipv4.conf.all.route_localnet=1
 sudo sysctl -w net.ipv4.ip_forward=1
 
+# ********************** #
+# CREATING BASE TEMPLATE #
+# ********************** #
+sudo DOWNLOAD_KEYSERVER="keyserver.ubuntu.com" lxc-create -n template -t download -- -d ubuntu -r focal -a amd64
+sudo lxc-start -n template
+
+# CREATE FAKE ADMIN USER
+sudo lxc-attach -n template -- bash -c "sudo useradd -m user"
+sudo lxc-attach -n template -- bash -c "echo user:password | sudo chpasswd"
+
+# INSTALL SSH
+sudo lxc-attach -n template -- bash -c "sudo apt-get update"
+sleep 10
+sudo lxc-attach -n template -- bash -c "sudo apt-get install openssh-server -y"
+sleep 10
+sudo lxc-attach -n template -- bash -c "sudo systemctl enable ssh --now"
+
+# ADD HONEY TO TEMPLATE
+sudo cp -r /home/honey/static/fall2021 /home/honey/static/spring2022 /var/lib/lxc/template/rootfs/home/user
+
+# INSTALL SNOOPY KEYLOGGER
+sudo lxc-attach -n template -- bash -c "sudo apt-get install wget -y"
+sleep 10
+sudo lxc-attach -n template -- bash -c "sudo wget -O install-snoopy.sh https://github.com/a2o/snoopy/raw/install/install/install-snoopy.sh"
+sudo lxc-attach -n template -- bash -c "sudo chmod 755 install-snoopy.sh"
+sudo lxc-attach -n template -- bash -c "sudo ./install-snoopy.sh stable"
+sudo lxc-attach -n template -- bash -c "sudo rm -rf ./install-snoopy.* snoopy-*" 
+# sudo lxc-attach -n template -- bash -c "wget -O /tmp/netdata-kickstart.sh https://my-netdata.io/kickstart.sh && sh /tmp/netdata-kickstart.sh --claim-token wceUolqqD-s5-CjqnwBUOSIZq6pyjwyDlal6eUF3l9uiucH3g9IdrUnfFRhpstkcHaiJm5hjgPAH1YPvXM3DwVk9Y66ed7EKOh3NJDezI_Jtjvk_ichHP9jnD3mWCjh-5m35byI --claim-url https://app.netdata.cloud"
+
+# *********************** #
+# CREATE BANNER TEMPLATES #
+# *********************** #
+
 LENGTH=4
 for ((j = 0 ; j < $LENGTH; j++));
 do
-	
-	ext_ip=${ips[$j]}
-	template=${templates[$j]}
 	scenario=${scenarios[$j]}
-	n="${scenario}_${ext_ip}" # name of the honeypot being deployed
-	mask=32
-
-	# stops and destroys if template already exists
-	exists=$(sudo lxc-ls -1 | grep -w $template | wc -l)
-	if [ $exists -ne 0 ]
-	then
-		sudo lxc-stop $template
-		sudo lxc-destroy $template
-	fi 
-
-	# CREATING SCENARIO TEMPLATE	
-	sudo DOWNLOAD_KEYSERVER="keyserver.ubuntu.com" lxc-create -n $template -t download -- -d ubuntu -r focal -a amd64
-	sudo lxc-start -n $template
-
-	# CREATE FAKE ADMIN USER
-	sudo lxc-attach -n $template -- bash -c "sudo useradd -m user"
-	sudo lxc-attach -n $template -- bash -c "echo user:password | sudo chpasswd"
+	n="template_${scenario}"
 	
-	# INSTALL SSH
-	sudo lxc-attach -n $template -- bash -c "sudo apt-get update"
-	sleep 10
-	sudo lxc-attach -n $template -- bash -c "sudo apt-get install openssh-server -y"
-	sleep 10
-	sudo lxc-attach -n $template -- bash -c "sudo systemctl enable ssh --now"
-
-	# ADD HONEY TO TEMPLATE
-	sudo cp -r /home/honey/static/fall2021 /home/honey/static/spring2022 /var/lib/lxc/$template/rootfs/home/user
-
-	# INSTALL SNOOPY KEYLOGGER
-	# logs to /var/log/snoopy.log within the container
-	# logs to /var/lib/lxc/$1/rootfs/var/log/snoopy.log on the host system
-	sudo lxc-attach -n $template -- bash -c "sudo apt-get install wget -y"
-	sleep 10
-	sudo lxc-attach -n $template -- bash -c "sudo wget -O install-snoopy.sh https://github.com/a2o/snoopy/raw/install/install/install-snoopy.sh"
-	sudo lxc-attach -n $template -- bash -c "sudo chmod 755 install-snoopy.sh"
-	sudo lxc-attach -n $template -- bash -c "sudo ./install-snoopy.sh stable"
-	sudo lxc-attach -n $template -- bash -c "sudo rm -rf ./install-snoopy.* snoopy-*" 
-	# sudo lxc-attach -n $template -- bash -c "wget -O /tmp/netdata-kickstart.sh https://my-netdata.io/kickstart.sh && sh /tmp/netdata-kickstart.sh --claim-token wceUolqqD-s5-CjqnwBUOSIZq6pyjwyDlal6eUF3l9uiucH3g9IdrUnfFRhpstkcHaiJm5hjgPAH1YPvXM3DwVk9Y66ed7EKOh3NJDezI_Jtjvk_ichHP9jnD3mWCjh-5m35byI --claim-url https://app.netdata.cloud"
+	# CREATE COPY OF BASE TEMPLATE
+	sudo lxc-copy -n template -N $n
 
 	# ADD WARNING BANNER
-	cat "/home/honey/static/warnings/$scenario.txt" | sudo tee -a /var/lib/lxc/$template/rootfs/etc/motd > /dev/null
-	sudo lxc-stop -n $template
+	cat "/home/honey/static/warnings/$scenario.txt" | sudo tee -a /var/lib/lxc/$n/rootfs/etc/motd > /dev/null
+	sudo lxc-stop -n $n		
+done
+
+# *********************** #
+# CREATE ACTUAL HONEYPOTS #
+# *********************** #
+
+LENGTH=4
+for ((j = 0 ; j < $LENGTH; j++));
+do
+	ext_ip=${ips[$j]}
+	scenario=${scenarios[$j]}
+	template="template_${scenario}"
+	n="${scenario}_${ext_ip}" # name of the honeypot being deployed
+	mask=32
 		
-	# CREATE HONEYPOT (COPY OF THE TEMPLATE)
+	# CREATE HONEYPOT 
 	sudo lxc-copy -n $template -N $n
 	sudo lxc-start -n $n
 	
@@ -77,12 +86,6 @@ do
 	container_ip=$(sudo lxc-info -n $n -iH)
 	echo "container: $n, container_ip: $container_ip, external_ip: $ext_ip"
 	
-	# SET UP HONEYPOT's FIREWALL RULES
-	#sudo ip link set enp4s2 up  
-	#sudo ip addr add $ext_ip/$mask brd + dev enp4s2
-	#sudo iptables --table nat --insert PREROUTING --source 0.0.0.0/0 --destination $ext_ip --jump DNAT --to-destination $container_ip
-	#sudo iptables --table nat --insert POSTROUTING --source $container_ip --destination 0.0.0.0/0 --jump SNAT --to-source $ext_ip 
-
 	# START HONEYPOT DATA COLLECTION
 	sudo /home/honey/tailing.sh $n $(date "+%F-%H-%M-%S")
 	
